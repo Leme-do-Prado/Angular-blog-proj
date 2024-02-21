1b3f65ed0a54ee5927fdad647f70b09efd89c443
leme-do-prado <urutuestrela@gmail.com>
2024-02-21 20:06:43 -0300

    Roteamento e dinamização iniciados!

---
+++ src/app/Data/datafake.ts
@@ -0,0 +1,14 @@
+export const dataMock=[
+  {
+"id":"1",
+"photo":"https://t2.tudocdn.net/307266?w=1920",
+"title": "Nova série",
+"description": "AAAAAAAAA"
+},
+{
+"id":"2",
+"photo":"https://t2.tudocdn.net/307266?w=1920",
+"title": "Filme em breve",
+"description": "Lorem ipsum"
+}
+]
